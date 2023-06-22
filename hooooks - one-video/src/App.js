d8a25a46022f5c1fe994e0dd539f3df07c56b21b
Prakash Kumar <132866106+PrakashDevnDsa@users.noreply.github.com>
2023-06-22 11:37:41 +0530

    Add files via upload

---
+++ hooooks - one-video/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import UseStatee from './Hooks/Usestatee';
+import UseEffect from './Hooks/UseEffect';
+import UseReducer from './Hooks/UseReducer';
+
+function App() {
+  return (
+//  <UseStatee/>
+//  <UseEffect/>
+<UseReducer/>
+
+  );
+}
+
+export default App;
