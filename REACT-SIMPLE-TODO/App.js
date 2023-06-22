e951ad6da5e79a99282a345e36d23f7f2c42b530
Prakash Kumar <132866106+PrakashDevnDsa@users.noreply.github.com>
2023-06-22 13:17:22 +0530

    Add files via upload

---
+++ REACT-SIMPLE-TODO/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Todo from './Todo';
+
+
+
+function App() {
+  return (
+//  <UseStatee/>
+//  <UseEffect/>
+// <UseReducer/> 
+<Todo/>
+  );
+}
+
+export default App;
